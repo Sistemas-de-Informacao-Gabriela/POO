42edb084dfb58e0e8ea1bbecd2643d01036d432a
Gabriela <gabedeandradee@gmail.com>
2023-05-08 15:26:41 -0300

    search com multiplos resultados

--- GUITARS PROJECT/class.py
+++ GUITARS PROJECT/class.py
@@ -68,6 +68,7 @@ class Inventory:
 # Este método é um pouco confuso... ele compara cada propriedade do objeto Guitar 
 # que é passado com cada objeto Guitar no inventário (estoque) do Rick
     def searchGuitar(self, searchGuitar):
+        guitars = []
         for guitar in self.guitars:
             # ignora o 'serialNumber' porque é único
             # ignora o 'price' porque é único
@@ -86,8 +87,8 @@ class Inventory:
             topWood = searchGuitar.getTopWood()
             if topWood is not None and topWood != "" and topWood != guitar.getTopWood():
                 continue
-            return guitar
-        return None
+            guitars.append(guitar)
+        return guitars
     
 
 # Testando o Sistema
@@ -97,15 +98,16 @@ inventory = Inventory()
 
 # Adiciona guitarras ao estoque
 inventory.addGuitar("V95693", 1499.95, Builder.FENDER.value, "Stratocastor", TypeG.ELETRIC.value, Wood.ALDER.value, Wood.ALDER.value)
+inventory.addGuitar("V95695", 1200.90, Builder.FENDER.value, "Stratocastor", TypeG.ELETRIC.value, Wood.ALDER.value, Wood.ALDER.value)
 #inventory.addGuitar("11277", 3999.95, "Collings", "CJ", "acoustic", "Indian Rosewood", "Indian Rosewood")
 
 
 # Buscando por uma guitarra que o Erin gosta: Fender Stratocastor elétrica com corpo de Alder e tampo de Alder
 whatErinLikes1 = Guitar("", 0, Builder.FENDER.value, "Stratocastor", TypeG.ELETRIC.value, Wood.ALDER.value, Wood.ALDER.value)
 
-guitar = inventory.searchGuitar(whatErinLikes1)
-if guitar:
-    print(f"Erin, you might like this {guitar.getBuilder()} {guitar.getModel()} {guitar.getTypeG()} guitar:\n{guitar.getBackWood()} back and sides,\n{guitar.getTopWood()} top.\nYou can have it for only ${guitar.getPrice()}!")
+guitars = inventory.searchGuitar(whatErinLikes1)
+for guitar in guitars:
+    if guitar:
+        print(f"Erin, you might like this {guitar.getBuilder()} {guitar.getModel()} {guitar.getTypeG()} guitar:\n{guitar.getBackWood()} back and sides,\n{guitar.getTopWood()} top.\nYou can have it for only ${guitar.getPrice()}!")
 else:
     print("Sorry, Erin, we have nothing for you.")
-
